3be260ee733a4fe9fee5c5836946c94d6c4283c4
Mauricio Azevedo <mauzevedo1979@gmail.com>
2021-06-26 07:33:45 -0300

    inicio do projeto

---
+++ cadastrar.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="css/estilo.css">
+    <title>Projeto Login</title>
+</head>
+<body>
+<div id="corpo-form-cad">
+    <h1>Cadastrar</h1>
+    <form method="POST" action="processa.php">
+        <input type="text" placeholder="Nome Completo">
+        <input type="text" placeholder="Telefone">
+        <input type="email" placeholder="Usuário">
+        <input type="password" placeholder="Senha">
+        <input type="password" placeholder="Confirmar Senha">
+        <input type="submit" value="CADASTRAR">
+    </form>
+</div>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="css/estilo.css">
+    <title>Projeto Login</title>
+</head>
+<body>
+<div id="corpo-form">
+    <h1>Entrar</h1>
+    <form method="POST" action="processa.php">
+        <input type="email" placeholder="Usuário">
+        <input type="password" placeholder="Senha">
+        <input type="submit" value="ACESSAR">
+        <a href="cadastrar.php">Ainda não é inscrito?<strong>Cadastre-se!</strong></a>
+    </form>
+</div>
+</body>
+</html>
