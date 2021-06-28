81de295d3039da42bb5aa2bf2de01e325c5ec8f7
Mauricio Azevedo <mauzevedo1979@gmail.com>
2021-06-27 22:05:37 -0300

    Alteração html

--- cadastrar.php
+++ cadastrar.php
@@ -2,20 +2,23 @@
 <html lang="pt-br">
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/estilo.css">
     <title>Projeto Login</title>
 </head>
+
 <body>
-<div id="corpo-form-cad">
-    <h1>Cadastrar</h1>
-    <form method="POST">
-        <input type="text" name="nome" placeholder="Nome Completo" maxlength="30">
-        <input type="text" name="telefone" placeholder="Telefone" maxlength="30">
-        <input type="email" name="email" placeholder="Usuário" maxlength="40">
-        <input type="password" name="senha" placeholder="Senha" maxlength="15">
-        <input type="password" name="confSenha" placeholder="Confirmar Senha" maxlength="15">
-        <input type="submit" value="CADASTRAR">
-    </form>
-</div>
-</body>
-</html>
+    <div class="backgorund">
+        <div class="container">
+            <form method="POST">
+                <h1>Cadastrar</h1>
+                <input class="pessoa" type="text" name="nome" placeholder="Nome Completo" maxlength="30">
+                <input class="telefone" type="text" name="telefone" placeholder="Telefone" maxlength="30">
+                <input class="email" type="email" name="email" placeholder="Usuário" maxlength="40">
+                <input class="password" type="password" name="senha" placeholder="Senha" maxlength="15">
+                <input class="password" type="password" name="confSenha" placeholder="Confirmar Senha" maxlength="15">
+                <input type="submit" value="CADASTRAR">
+            </form>
+        </div>
+    </div>
+</body></html>
--- index.php
+++ index.php
@@ -2,18 +2,25 @@
 <html lang="pt-br">
 <head>
     <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/estilo.css">
     <title>Projeto Login</title>
 </head>
+
 <body>
-<div id="corpo-form">
-    <h1>Entrar</h1>
-    <form method="POST">
-        <input type="email" name="email" placeholder="Usuário">
-        <input type="password" name="senha" placeholder="Senha">
-        <input type="submit" value="ACESSAR">
-        <a href="cadastrar.php">Ainda não é inscrito?<strong>Cadastre-se!</strong></a>
-    </form>
-</div>
-</body>
-</html>
+    <div class="backgorund">
+        <div class="container">
+            <form method="POST">
+                <h1>Entrar</h1>
+                <input class="email" type="email" name="email" placeholder="Usuário">
+                <input class="password" type="password" name="senha" placeholder="Senha">
+                <input type="submit" value="ACESSAR">
+                <a href="cadastrar.php">
+                    Ainda não é inscrito?
+                    <strong>Cadastre-se!</strong>
+                </a>
+            </form>
+        </div>
+    </div>
+</body></html>
+
